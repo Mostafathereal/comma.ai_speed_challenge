2a5f6cfe454097b3a52463401f3332c6c328d611
MostafaTheReal <reachmostafa.m@gmail.com>
2020-09-11 18:15:18 -0400

    update

--- of.py
+++ of.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 import numpy as np 
 import time
-from numpy import save
-from numpy import asarray
+from numpy import save, loadtxt
 
+labels = loadtxt('/home/mostafathereal/Desktop/comma.ai_speed_challenge/data/train.txt', delimiter = "\n")
 
-cap = cv.VideoCapture('/home/mostafathereal/Desktop/speed_challenge/data/test.mp4')
+cap = cv.VideoCapture('/home/mostafathereal/Desktop/comma.ai_speed_challenge/data/train.mp4')
 
 width  = cap.get(3)
 height = cap.get(4)
@@ -26,39 +26,50 @@ mask = np.zeros_like(first_frame)
 # Sets image saturation to maximum 
 mask[..., 1] = 255
 
+train_data = []
+label_data = []
 
-for i in range(0, 20390):
+for i in range(1, 20390):
 
-    ret, frame = cap.read()
-    cv.imshow('test input', frame)
+        ret, frame = cap.read()
+        cv.imshow('test input', frame)
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) 
-    
-    # Calculates dense qoptical flow by Farneback method 
-    # Computes the magnitude and angle of the 2D vectors
-    flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)  
-    
-    # angle in radians
-    magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1]) 
-    print(type(min(angle[0])))
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) 
 
-    # print(len(magnitude), len(magnitude[0]))
-    # print(len(angle), len(angle[0]))
-    # print("yeehaw")q
-    # Converts HSV to RGB (BGR) color representation 
-    mask[..., 0] = angle * 180 / np.pi / 2
-    mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX) 
-      
-    rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR) 
-      
-    # Opens a new window and displays the output frame 
-    cv.imshow("dense optical flow", rgb) 
-      
-    # Updates previous frame 
-    prev_gray = gray
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Calculates dense qoptical flow by Farneback method 
+        # Computes the magnitude and angle of the 2D vectors
+        flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)  
 
+        # angle in radians
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1]) 
+
+
+        # print(len(magnitude), len(magnitude[0]))
+        # print(len(angle), len(angle[0]))
+        # print(len(gray), len(gray[0]))
+
+        arr = [magnitude, angle, gray]
+        train_data.append(arr)
+        label_data.append(labels[i])
+
+        # print("yeehaw")q
+        # Converts HSV to RGB (BGR) color representation 
+        mask[..., 0] = angle * 180 / np.pi / 2
+        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX) 
+
+        rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR) 
+
+        # Opens a new window and displays the output frame 
+        cv.imshow("dense optical flow", rgb) 
+
+        # Updates previous frame 
+        prev_gray = gray
+        if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+save('/home/mostafathereal/Desktop/comma.ai_speed_challenge/data1/train.npy', train_data)
+save('/home/mostafathereal/Desktop/comma.ai_speed_challenge/data1/train_labels.npy', label_data)
 
 cap.release()
 cv.destroyAllWindows()
