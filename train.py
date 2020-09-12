d2b265ea76001d4a5a25db7476c98a9d888aad96
MostafaTheReal <reachmostafa.m@gmail.com>
2020-09-12 14:52:20 -0400

    training loop

---
+++ train.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+from inception_tl import *
+
+cuda = torch.device("cuda")
+
+speedNet = InceptionSpeed().to(cuda)
+
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.AdamW(speedNet.parameters(), lr = 0.001, betas = (0.9, 0.999))
+
+inputs = torch.load("/home/mostafathereal/Desktop/comma.ai_speed_challenge/data1/train.pt")
+labels = torch.load("/home/mostafathereal/Desktop/comma.ai_speed_challenge/data1/train_labels.pt")
+
+inputs = inputs.type(torch.cuda.FloatTensor)
+
+for epoch in range(30):
+
+    running_loss = 0.0
+    for i in range(len(labels)):
+
+        optimizer.zero_grad()
+
+        # print(np.shape((inputs[i])))
+        speedNet.eval()
+        outputs = speedNet(inputs[i])
+        speedNet.train()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/(i+1)))
+
+
+print("finished training")
